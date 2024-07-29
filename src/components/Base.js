180628f15407fb917af588802869c96e654c0377
BuildTools <unconfigured@null.spigotmc.org>
2024-07-28 20:19:22 -0400

    Fixed CSRF token issues

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 const Base = () => {
+  useEffect(() => {
+    const fetchCsrfToken = async() => {
+      try {
+        const response = await axios.get('http://localhost:8000/csrf/', { withCredentials: true });
+        const csrfToken = response.data.csrfToken;
+        Cookies.set('csrftoken', csrfToken);
+      } catch (error) {
+        console.error('Error fetching CSRF token:', error);
+      }
+    };
+
+    fetchCsrfToken();
+  }, []);
+
   return (
     <Navbar expand="lg" className="spotify-navbar">
       <Container>
